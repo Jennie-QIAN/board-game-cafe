1e4441d3ffbbb3ffbe14375215c9f43d60fb6e1b
Zhenni Qian <zhenni.qian913@gmail.com>
2020-12-10 00:21:55 +0100

    adjusted seed.js so that connection is closed when collection seeded

--- bin/seed.js
+++ bin/seed.js
@@ -18,19 +18,19 @@ const gameIdParam = "thing?id=";
 
 function getObjectFromBgg(url) {
     return axios.get(url)
-    .then(res => {
-        const options = {
-            object: false,
-            reversible: false,
-            coerce: false,
-            sanitize: true,
-            trim: true,
-            arrayNotation: false,
-            alternateTextNode: false
-        };
-        return JSON.parse(parser.toJson(res.data, options)).items.item;
-    })
-    .catch(err => console.log(err));
+        .then(res => {
+            const options = {
+                object: false,
+                reversible: false,
+                coerce: false,
+                sanitize: true,
+                trim: true,
+                arrayNotation: false,
+                alternateTextNode: false
+            };
+            return JSON.parse(parser.toJson(res.data, options)).items.item;
+        })
+        .catch(err => console.log(err));
 }
 
 function getPropertyValueFromApi(property, array) {
@@ -39,47 +39,51 @@ function getPropertyValueFromApi(property, array) {
 
 getObjectFromBgg(baseBggApiUrl + collectionParam)
     .then(bggCollection => bggCollection.map(game => game.objectid))
-    .then(bggGameIds => bggGameIds.forEach(bggGameId => {
-        getObjectFromBgg(baseBggApiUrl + gameIdParam + bggGameId)
-            .then(data => {
-                const { 
-                    name: nameList, 
-                    id: bggId, 
-                    thumbnail: smImg, 
-                    image: img,
-                    minplayers,
-                    maxplayers,
-                    playingtime,
-                    yearpublished,
-                    link
-                } = data;
+    .then(bggGameIds => {
+        const gamesPromises = bggGameIds.map(bggGameId => {
+            return getObjectFromBgg(baseBggApiUrl + gameIdParam + bggGameId)
+                .then(data => {
+                    const { 
+                        name: nameList, 
+                        id: bggId, 
+                        thumbnail: smImg, 
+                        image: img,
+                        minplayers,
+                        maxplayers,
+                        playingtime,
+                        yearpublished,
+                        link
+                    } = data;
 
-                const designer = getPropertyValueFromApi("boardgamedesigner", link);
-                const artist = getPropertyValueFromApi("boardgameartist", link);
-                const publisher = getPropertyValueFromApi("boardgamepublisher", link);
-                const category = getPropertyValueFromApi("boardgamecategory", link);
-                const mechanic = getPropertyValueFromApi("boardgamemechanic", link);
+                    const designer = getPropertyValueFromApi("boardgamedesigner", link);
+                    const artist = getPropertyValueFromApi("boardgameartist", link);
+                    const publisher = getPropertyValueFromApi("boardgamepublisher", link);
+                    const category = getPropertyValueFromApi("boardgamecategory", link);
+                    const mechanic = getPropertyValueFromApi("boardgamemechanic", link);
 
-                const game = {
-                    name: nameList[0].value,
-                    bggId,
-                    smImg,
-                    img,
-                    minPlayer: minplayers.value,
-                    maxPlayer: maxplayers.value,
-                    gamePlayTime: playingtime.value,
-                    yearOfPublish: yearpublished.value,
-                    designer,
-                    artist,
-                    publisher,
-                    category,
-                    mechanic
-                };
+                    const game = {
+                        name: nameList[0].value,
+                        bggId,
+                        smImg,
+                        img,
+                        minPlayer: minplayers.value,
+                        maxPlayer: maxplayers.value,
+                        gamePlayTime: playingtime.value,
+                        yearOfPublish: yearpublished.value,
+                        designer,
+                        artist,
+                        publisher,
+                        category,
+                        mechanic
+                    };
 
-                Game.create(game)
-                    .then()
-                    .catch(err => console.log(`An error occurred while creating a game fetched from BGG API: ${err}`));
-            })
-            .catch(err => console.log(err));
-    }))
+                    return Game.create(game).catch(err => console.log(`An error occurred while creating a game fetched from BGG API: ${err}`));
+                })
+
+                .catch(err => console.log(err));
+        });
+
+        return Promise.all(gamesPromises);
+    })
+    .then(() => mongoose.connection.close())
     .catch(err => console.log(err));
