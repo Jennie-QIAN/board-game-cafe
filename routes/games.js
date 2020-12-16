d5a244aa3ab6226b5483bca75a39629c3187d159
Zhenni Qian <zhenni.qian913@gmail.com>
2020-12-17 00:08:23 +0100

    changed to GET method for both of the form submits for /games

--- routes/games.js
+++ routes/games.js
@@ -5,26 +5,8 @@ const { findAllGames } = require('../queries/games.query');
 const Game = require('../models/Game.model.js');
 
 router.get('/games', async (req, res, next) => {
+
     const { game, category, mechanic, minPlayer, maxPlayer } = req.query;
-
-    const [ categories, mechanics, games  ] = await Promise.all([
-        Game.distinct('category'),
-        Game.distinct('mechanic'),
-        findAllGames(game, category, mechanic, minPlayer, maxPlayer)
-      ]);
-
-    res.render('games/games', {
-        game,
-        isLoggedIn: req.isAuthenticated(),
-        games,
-        categories,
-        mechanics
-    });
-});
-
-router.post('/games', async (req, res, next) => {
-
-    const { game, category, mechanic, minPlayer, maxPlayer } = req.body;
 
     const [ categories, mechanics, games  ] = await Promise.all([
         Game.distinct('category'),
